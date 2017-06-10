94cacf768159426660b62230e4fa1daf8d5edbd5
YIRer <xeroonelove@gmail.com>
2017-06-10 11:57:54 +0900

    asd

---
+++ typescript-basic/hello/helloWorld.js
@@ -0,0 +1,5 @@
+console.log("hello world");
+function hello(string) {
+    console.log("hell" + string);
+}
+hello("john");
---
+++ typescript-basic/hello/helloWorld.ts
@@ -0,0 +1,6 @@
+console.log("hello world");
+function hello(string : string){
+  console.log("hell" + string);
+}
+
+hello("john");
